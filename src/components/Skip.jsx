9a3c29f77cd3b6ab767f51569601eef9ad3b877b
leonardorexkhan <leonardo.rexkhan@gmail.com>
2025-09-01 14:12:29 +0900

    Add files via upload

---
+++ src/components/Skip.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Skip = () => {
+  return (
+      <section id='skip'>
+        <a href="/">Intro</a>
+        <a href="/">Skill</a>
+        <a href="/">Site</a>
+        <a href="/">Port</a>
+        <a href="/">Contact</a>
+      </section>
+  )
+}
+
+export default Skip
